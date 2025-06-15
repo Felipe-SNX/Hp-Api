58b1bbe8ce6ef5d6f6f5bee25ae1c7e8b37ec067
gabriel <gabrielsilva.ddds@gmail.com>
2025-06-15 17:19:26 -0300

    tela principal

--- app/src/main/java/com/example/hp_api/controller/MainActivity.kt
+++ app/src/main/java/com/example/hp_api/controller/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.hp_api.controller
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -8,14 +10,36 @@ import androidx.core.view.WindowInsetsCompat
 import com.example.hp_api.R
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            val sys = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(sys.left, sys.top, sys.right, sys.bottom)
             insets
         }
+
+        val btnPersonagem   = findViewById<Button>(R.id.btnPersonagem)
+        val btnProfessores  = findViewById<Button>(R.id.btnProfessores)
+        val btnEstudantes   = findViewById<Button>(R.id.btnEstudantesCasa)
+        val btnFeiticos     = findViewById<Button>(R.id.btnFeiticos)
+        val btnSair         = findViewById<Button>(R.id.btnSair)
+
+        btnPersonagem.setOnClickListener {
+            startActivity(Intent(this, ListCharacter::class.java))
+        }
+        btnProfessores.setOnClickListener {
+            startActivity(Intent(this, ListTeacher::class.java))
+        }
+        btnEstudantes.setOnClickListener {
+            startActivity(Intent(this, ListStudents::class.java))
+        }
+        btnFeiticos.setOnClickListener {
+            startActivity(Intent(this, ListSpells::class.java))
+        }
+        btnSair.setOnClickListener { finishAffinity() }
     }
 }
