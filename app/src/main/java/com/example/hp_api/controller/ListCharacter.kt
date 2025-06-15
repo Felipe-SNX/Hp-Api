84679ec7cae06953b08faf8d0ae2c79cb409d0d2
gabriel <gabrielsilva.ddds@gmail.com>
2025-06-15 19:30:36 -0300

    list studens

--- app/src/main/java/com/example/hp_api/controller/ListCharacter.kt
+++ app/src/main/java/com/example/hp_api/controller/ListCharacter.kt
@@ -40,9 +40,6 @@ class ListCharacter : AppCompatActivity() {
                     val response = RetrofitClient.instance.getPersonagem()
             }
         }*/
-
-
-
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
