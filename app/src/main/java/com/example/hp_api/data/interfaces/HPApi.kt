f404bc665a5fa0626c65f182ba4fb0e6b74bf3e0
Luiz Bussolo <bussolo@ufpr.br>
2025-06-15 19:08:39 -0300

    feat: Listar Personagem por ID (wip)

--- app/src/main/java/com/example/hp_api/data/interfaces/HPApi.kt
+++ app/src/main/java/com/example/hp_api/data/interfaces/HPApi.kt
@@ -1,9 +1,15 @@
 package com.example.hp_api.data.interfaces
 
 import com.example.hp_api.model.FeiticosModel
+import com.example.hp_api.model.PersonagemModel
 import retrofit2.http.GET
+import retrofit2.http.Path
 
 interface HPApi {
     @GET("spells")
     suspend fun getAllSpells(): List<FeiticosModel>;
+
+    @GET("character/{id}")
+    suspend fun getPersonagemID(@Path("id") id: String): List<PersonagemModel>
+
 }
